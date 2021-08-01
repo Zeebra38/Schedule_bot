b887bd6ad7e5f1a68aa55ba696cb6901875c42a6
Zeebra38 <darios2013@bk.ru>
2021-08-01 13:43:33 +0300

    Класс для создания предмета готов. Начата работа над класс, в который будут поступать несколько предметов

--- Subjest class.py
+++ Subjest class.py
@@ -1,3 +1,52 @@
+import re
+
+
 class Subject:
+    """
+    group_name - название группы str, number - порядкой номер int, even - четность (1/0) int, 
+    subj - предмет вместе с номерами недель str, _type - тип str,
+    instructor - преподаватель str, _class  - аудитория str, link - ссылка str
+    """
+
+    def __init__(self, group_name, number, even, subj, _type, instructor, _class, link):
+        self.group_name = group_name
+        self.number = number
+        self.even = even
+        self._type = _type
+        self.instructor = instructor
+        self._class = _class
+        self.link = link
+        self.name, self.weeks = self.set_up_subject(subj, even)
+
+    def set_up_subject(self, subj, even):
+        pattern = r"[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?"
+        odd_weeks = '1 3 5 7 9 11 13 15'.split()
+        even_weeks = '2 4 6 8 10 12 14 16'.split()
+        started_weeks = re.findall(pattern, subj)
+        if len(started_weeks) == 0:
+            if even:
+                weeks = even_weeks
+            else:
+                weeks = odd_weeks
+            name = subj
+        else:
+            if re.match(r'^кр', subj):
+                if even:
+                    set2 = set(started_weeks)
+                    weeks = [o for o in even_weeks if o not in set2]
+                else:
+                    set2 = set(started_weeks)
+                    weeks = [o for o in odd_weeks if o not in set2]
+            else:
+                weeks = started_weeks
+            name = subj[subj.find('н') + 1:].strip()
+
+        self.weeks = weeks
+        return name, weeks
+
+
+class MultiSubject:
+    """Используется, когда в строке несколько различных предметов"""
+
     def __init__(self):
         pass
